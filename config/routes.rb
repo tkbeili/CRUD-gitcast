26aaab4adef99bbaf98828076d6013d7a25395b0
Tammam Kbeili <tammam.kbeili@gmail.com>
2015-06-15 08:37:54 -0700

    Implement the users controller actions
    We now implement the new and create actions for the users controller. We first make sure to have the routes set up by adding `resources :users, only: [:new, :create]`. Then we implement the new and create actions as standard ones with Rails CRUD. After that we implement the `new.html.erb`template.
    
    It's generally a good idea to make sure that we have the `root_path` setup. In this case we make it go to the `index` action in the `posts` controller.
    
    The only difference from a standard CRUD is that after the users successfully create their user record we add `sessions[:user_id] = @user.id`. This way we authenticate the user to our website using the session upon successful account creation.

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
 
+  resources :users, only: [:new, :create]
+
   resources :posts
+
+  root "posts#index"
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
